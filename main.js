515329dacc1f002f688722a454b37db66523e2f1
Ushieru Kokoran <uzielcocolan@gmail.com>
2020-07-02 12:42:56 -0500

    example

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const Datastore = require('nedb');
 
-function createWindow () {
+var database;
+
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
     }
   })
 
@@ -24,7 +28,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -37,7 +41,26 @@ app.whenReady().then(() => {
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+ipcMain.on('create-database', (event, arg) => {
+  database = new Datastore({ filename: `${arg}.db`, autoload: true });
+  event.reply('create-database-reply', 'BB Creada')
+});
+
+ipcMain.on('create-user', (event, arg) => {
+  var doc = {
+    name: arg.user,
+    password: arg.password
+  };
+
+  database.insert(doc, function (err, newDoc) {
+    if (err)
+      event.reply('create-user-reply', 'Error al Crear Usuario');
+
+    if (newDoc._id)
+      event.reply('create-user-reply', 'Usuario Creado');
+  });
+});
--- renderer.js
+++ renderer.js
@@ -4,3 +4,29 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const { ipcRenderer } = require('electron')
+
+let createDB = document.getElementById("createDataBase");
+let createUser = document.getElementById("createUser");
+
+var user = document.getElementById("user");
+var password = document.getElementById("password");
+
+// LAUNCH LISTENERS WITH EVENTS
+createDB.addEventListener("click", function () {
+    ipcRenderer.send('create-database', 'users');
+});
+
+createUser.addEventListener("click", function () {
+    ipcRenderer.send('create-user', { user: user.value, password: password.value });
+    user.value = ""; password.value = "";
+});
+
+//OPEN LISTENERS
+ipcRenderer.on('create-database-reply', (event, response) => {
+    console.log(response);
+});
+
+ipcRenderer.on('create-user-reply', (event, response) => {
+    console.log(response);
+});
